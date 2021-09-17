105ac0b45d774cfb7af2bbfe6eccdcf5c1eb5d19
Ahmed Gamal <63549938+AhmedGamal710@users.noreply.github.com>
2021-09-17 23:04:31 +0100

    first commit

---
+++ .vscode/main.js
@@ -0,0 +1,10 @@
+/// <reference path="c:/Users/A.Gamal/.vscode/extensions/nur.script-0.2.1/@types/api.global.d.ts" />
+/// <reference path="c:/Users/A.Gamal/.vscode/extensions/nur.script-0.2.1/@types/vscode.global.d.ts" />
+
+function activate(_context) {
+   window.showInformationMessage('Hello, World!');
+}
+
+function deactivate() {}
+
+module.exports = { activate, deactivate }
